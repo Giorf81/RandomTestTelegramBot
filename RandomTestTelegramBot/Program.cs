49b3d8b0c3fdcd5bca490d2f654ae9c3ee1dc776
Giorf81 <chellcarolane@gmail.com>
2025-03-16 20:57:28 +0300

    Добавьте файлы проекта.

---
+++ RandomTestTelegramBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RandomTestTelegramBot;
+using Telegram.Bot;
+
+namespace RandomTestTelegramBot
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder().ConfigureServices((hostContext, services) => ConfigureServices(services)).UseConsoleLifetime().Build();
+
+            Console.WriteLine("Сервис запущен!");
+
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен!");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("7123537005:AAEsa7x7NAdP_sQMfRwHvsOcoK5IZmI8T8s"));
+            services.AddHostedService<Bot>();
+        }
+    }
+}
